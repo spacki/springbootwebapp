0a0d6d9504c10bf6dc5c5af5ec4f675d95bd2517
Karsten Spakowski <kspakowski@web.de>
2017-03-21 08:18:44 +0100

    new domain

--- src/main/java/guru/springframework/domain/Radiologist.java
+++ src/main/java/guru/springframework/domain/Radiologist.java
@@ -4,12 +4,117 @@ package guru.springframework.domain;
  * Created by karstenspakowski on 21/03/17.
  */
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity 
 public class Radiologist {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    String userName;
+
+    private String domain;
+
+    private String lastname;
+
+    private String firstName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String department;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
 
-
+    @Override
+    public String toString() {
+        return "Radiologist{" +
+                "id=" + id +
+                ", version=" + version +
+                ", userName='" + userName + '\'' +
+                ", domain='" + domain + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
 }
